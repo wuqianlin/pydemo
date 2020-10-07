d806f326c1a21498b8a659f1ee4f11af09b13fd1
QianlinWu <simpleran@sina.com>
2020-10-07 10:57:24 +0800

    使用特性获取链接记录

--- fluentpython/explore0.py
+++ fluentpython/explore0.py
@@ -5,6 +5,13 @@ import keyword
 class FrozenJSON:
     """一个只读接口，使用属性表示法访问JSON类对象
     """
+    def __new__(cls, arg):
+        if isinstance(arg, abc.Mapping):
+            return super().__new__(cls)
+        elif isinstance(arg, abc.MutableSequence):
+            return [cls(item) for item in arg]
+        else:
+            return arg
 
     def __init__(self, mapping):
         self.__data = {}
@@ -17,13 +24,4 @@ class FrozenJSON:
         if hasattr(self.__data, name):
             return getattr(self.__data, name)
         else:
-            return FrozenJSON.build(self.__data[name])
-
-    @classmethod
-    def build(cls, obj):
-        if isinstance(obj, abc.Mapping):
-            return cls(obj)
-        elif isinstance(obj, abc.MutableSequence):
-            return [cls.build(item) for item in obj]
-        else:
-            return obj
+            return FrozenJSON(self.__data[name])
