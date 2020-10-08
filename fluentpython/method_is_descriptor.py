a880a4c4acaf3aca19b3e5a855f67ed2aaf711c3
QianlinWu <simpleran@sina.com>
2020-10-08 15:53:57 +0800

    覆盖型与非覆盖型描述符对比

---
+++ fluentpython/method_is_descriptor.py
@@ -0,0 +1,10 @@
+import collections
+
+
+class Text(collections.UserString):
+
+    def __repr__(self):
+        return 'Text({!r})'.format(self.data)
+
+    def reverse(self):
+        return self[::-1]
