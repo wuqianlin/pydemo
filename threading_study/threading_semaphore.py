f7cf402057eb1980dc4d7cb397c62a6365f05f22
QianlinWu <simpleran@sina.com>
2021-01-28 22:54:22 +0800

    增加 threading semaphore 使用

---
+++ threading_study/threading_semaphore.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+import threading
+import time
+import random
+
+semaphore = threading.Semaphore(0)
+item = 0
+
+
+def consumer():
+    print("Consumer is waiting.")
+    semaphore.acquire()
+    print("Consumer notify: Consumed item number %s" % item)
+
+
+def producer():
+    global item
+    time.sleep(2)
+    item = random.randint(0, 100)
+    print("Producer notify: Produced item number %s" % item)
+    semaphore.release()
+
+
+if __name__ == "__main__":
+    for i in range(0, 5):
+        t1 = threading.Thread(target=producer)
+        t2 = threading.Thread(target=consumer)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+    print("Program terminated")
