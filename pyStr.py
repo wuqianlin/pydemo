17fe86cde6a0e6b66ebad8a5c80d8cadcd9f31cc
QianlinWu <simpleran@sina.com>
2021-01-29 13:58:20 +0800

    python 字符串书写规则

---
+++ pyStr.py
@@ -0,0 +1,19 @@
+s = """fuck\
+a\
+b\
+c"""
+print(type(s), len(s), s)
+
+
+b = """fuck 
+a 
+         b 
+c"""
+print(type(b), len(b), b)
+
+
+c = ('fuck'
+     'a'
+     'b'
+     'c')
+print(type(c), len(c), c)
