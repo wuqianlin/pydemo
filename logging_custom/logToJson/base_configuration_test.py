470f6bb030d416200a47d1c4c3f74c9ed8259001
QianlinWu <simpleran@sina.com>
2021-01-29 11:15:54 +0800

    logging custom: log to json

---
+++ logging_custom/logToJson/base_configuration_test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import logging
+from logging.config import fileConfig
+
+
+def base_configuration():
+    fileConfig('log_conf.ini')
+    log = logging.getLogger(__name__)
+
+    try:
+        log.info({'a': 123})
+        # log.info("message2", {"extra_info": " trace info"})
+        # log.info({"message3": " log info"})
+        a = 1 / 0
+    except Exception:
+        # log.error("occurred exception", exc_info=True)
+        log.exception({"exception_id": 22000})
+        # log.exception("occurred exception")
+        # log.exception("occurred exception", {"exception_id":225462})
+
+
+base_configuration()
